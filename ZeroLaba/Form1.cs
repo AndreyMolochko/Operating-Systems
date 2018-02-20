996048f71a62aca6f130732e9fae0533b1796729
krok1999 <molochko.andrey@mail.ru>
2018-02-20 22:32:46 +0300

    Refactoring some functions

--- ZeroLaba/Form1.cs
+++ ZeroLaba/Form1.cs
@@ -27,18 +27,19 @@ namespace ZeroLaba
 
         private void button1_Click(object sender, EventArgs e)
         {
-            long counterSizeMemory=0;
+            long counterSizeMemory = 0;
             Process ps;
             for (int i = 0; i < listProcesses.Length; i++)
             {
-                
-                listBox1.Items.Add("name = " + listProcesses[i].ProcessName.ToString() + "   id =  " + listProcesses[i].Id.ToString() + "    thread = " +
-                    getCountThread(listProcesses[i].Id).ToString()+"    sizeVirtualMemory = "+
-                    (listProcesses[i].PagedMemorySize64/1024).ToString() + "( kb )" +
-                    "  sizePhysicalMemory  = " + listProcesses[i].WorkingSet64/1024 + "  (kb)" +
-                    "  curPrioritet(T) = "+listProcesses[i].BasePriority+
-                    "  id(T)");
-                
+
+                listBox1.Items.Add("name = " + getName(i) +
+                    " id = " + getId(i) +
+                    " thread = " + getCountThreads(listProcesses[i].Id) +
+                    " sizeVirtualMemory = " + getSizeVirtMemory(i) + " ( kb )" +
+                    " sizePhysicalMemory = " + getSizePhysicMemory(i) + " ( kb ) " +
+                    " curPrioritet(T) = " + getCurPrioritet(i) +
+                    " idThread = ");
+
             }
             label1.Text = "Current progresses " + listProcesses.Length;
             //label1.Text= 
@@ -46,6 +47,39 @@ namespace ZeroLaba
             //listBox1.Items.Add(listProcesses[0]);
 
         }
+        public String getName(int i)
+        {
+            return listProcesses[i].ProcessName.ToString();
+        }
+
+        public String getId(int i)
+        {
+            return listProcesses[i].Id.ToString();
+        }
+
+        public String getCountThreads(int processId)
+        {
+            int count = 0;
+            Process currentProcess = Process.GetProcessById(processId);
+            count = currentProcess.Threads.Count;
+            return count.ToString();
+        }
+
+        public String getSizeVirtMemory(int i)
+        {
+            return (listProcesses[i].PagedMemorySize64 / 1024).ToString();
+        }
+
+        public String getSizePhysicMemory(int i)
+        {
+            return (listProcesses[i].WorkingSet64 / 1024).ToString();            
+        }
+
+        public String getCurPrioritet(int i)
+        {
+            return listProcesses[i].BasePriority.ToString();
+        }
+        
 
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -56,12 +90,6 @@ namespace ZeroLaba
         {
 
         }
-        public int getCountThread(int processId)
-        {
-            int count = 0;
-            Process currentProcess = Process.GetProcessById(processId);
-            count = currentProcess.Threads.Count;            
-            return count;            
-        }        
+        
     }
 }
