5188cb30999148ec6219cf748618a2d3347260e6
AndreyMolochko <Molochko_AG_16@student.grsu.by>
2018-02-21 19:14:15 +0300

    All methods fulfilled

--- ZeroLaba/Form1.Designer.cs
+++ ZeroLaba/Form1.Designer.cs
@@ -31,6 +31,9 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -45,7 +48,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 283);
+            this.button1.Location = new System.Drawing.Point(12, 281);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(184, 23);
             this.button1.TabIndex = 2;
@@ -58,15 +61,45 @@
             this.listBox1.FormattingEnabled = true;
             this.listBox1.Location = new System.Drawing.Point(12, 50);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(1019, 225);
+            this.listBox1.Size = new System.Drawing.Size(840, 225);
             this.listBox1.TabIndex = 3;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(876, 50);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 225);
+            this.listBox2.TabIndex = 4;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(1002, 50);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(120, 225);
+            this.listBox3.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(202, 281);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(184, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1211, 492);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
@@ -81,6 +114,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- ZeroLaba/Form1.cs
+++ ZeroLaba/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Management;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -27,21 +28,7 @@ namespace ZeroLaba
 
         private void button1_Click(object sender, EventArgs e)
         {
-            long counterSizeMemory = 0;
-            Process ps;
-            for (int i = 0; i < listProcesses.Length; i++)
-            {
-
-                listBox1.Items.Add("name = " + getName(i) +
-                    " id = " + getId(i) +
-                    " thread = " + getCountThreads(listProcesses[i].Id) +
-                    " sizeVirtualMemory = " + getSizeVirtMemory(i) + " ( kb )" +
-                    " sizePhysicalMemory = " + getSizePhysicMemory(i) + " ( kb ) " +
-                    " curPrioritet(T) = " + getCurPrioritet(i) +
-                    " idThread = ");
-
-            }
-            label1.Text = "Current progresses " + listProcesses.Length;
+            mainFunction();
             //label1.Text= 
 
             //listBox1.Items.Add(listProcesses[0]);
@@ -60,9 +47,13 @@ namespace ZeroLaba
         public String getCountThreads(int processId)
         {
             int count = 0;
-            Process currentProcess = Process.GetProcessById(processId);
-            count = currentProcess.Threads.Count;
-            return count.ToString();
+
+            try { Process currentProcess = Process.GetProcessById(processId);
+                count = currentProcess.Threads.Count;
+                return count.ToString();
+            }
+            catch { return "-----"; }
+
         }
 
         public String getSizeVirtMemory(int i)
@@ -72,7 +63,7 @@ namespace ZeroLaba
 
         public String getSizePhysicMemory(int i)
         {
-            return (listProcesses[i].WorkingSet64 / 1024).ToString();            
+            return (listProcesses[i].WorkingSet64 / 1024).ToString();
         }
 
         public String getCurPrioritet(int i)
@@ -82,14 +73,71 @@ namespace ZeroLaba
 
         public String[] getPrioriteThreads(int i)
         {
-            String[] arr = new String [listProcesses[i].Threads.Count];
-            for(int j = 0; j < arr.Length; j++)
+            String[] arr = new String[listProcesses[i].Threads.Count];
+            for (int j = 0; j < arr.Length; j++)
             {
-                arr[j] = listProcesses[i].Threads[j].BasePriority.ToString();
+                arr[j] = "prior = " + listProcesses[i].Threads[j].CurrentPriority.ToString();
             }
             return arr;
         }
+
+        public String[] getIdThreads(int i)
+        {
+            String[] arr = new String[listProcesses[i].Threads.Count];
+            for (int j = 0; j < arr.Length; j++)
+            {
+                arr[j] = "  id = " + listProcesses[i].Threads[j].Id.ToString();
+            }
+            return arr;
+        }
+
+        public String getCurrentPrior(int i) {
+            String str;
+            try { return listProcesses[i].PriorityClass.ToString(); }
+            catch
+            {
+                return "------";
+            }
+        }
+        public string getProcessOwner(int processId)
+        {
+            string query = "Select * From Win32_Process Where ProcessID = " + processId;
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+            ManagementObjectCollection processList = searcher.Get();
+
+            foreach (ManagementObject obj in processList)
+            {
+                string[] argList = new string[2];
+                int returnVal = Convert.ToInt32(obj.InvokeMethod("GetOwner", argList));
+                if (returnVal == 0)
+                {
+                    // return DOMAIN\user
+                    return argList[1] + "\\" + argList[0];
+                }
+            }
+
+            return "There isn't owner";
+        }        
+
+        public void mainFunction()
+        {
+            long counterSizeMemory = 0;
+            Process ps;
+            for (int i = 0; i < listProcesses.Length; i++)  
+                listBox1.Items.Add("name = " + getName(i) +
+                    " id = " + getId(i) +
+                    " thread = " + getCountThreads(listProcesses[i].Id) +
+                    " sizeVirtualMemory = " + getSizeVirtMemory(i) + " ( kb )" +
+                    " sizePhysicalMemory = " + getSizePhysicMemory(i) + " ( kb ) " +
+                    //" curPrioritet(T) = " + getCurPrioritet(i) +
+                    " curPrioritet(P) = " + getCurrentPrior(i) +
+                    " owner = " + getProcessOwner(listProcesses[i].Id));
+            label1.Text = "Current progresses " + listProcesses.Length;
+        }       
+    
+            
         
+
 
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -98,8 +146,24 @@ namespace ZeroLaba
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            listBox2.Items.Clear();
+            listBox2.Items.AddRange(getPrioriteThreads(listBox1.SelectedIndex));
+            listBox3.Items.Clear();
+            listBox3.Items.AddRange(getIdThreads(listBox1.SelectedIndex));
+            
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
 
         }
-        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+            mainFunction();
+        }
     }
 }
